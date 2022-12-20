0f657c6469c817115eeaaaea36b2bb2356502267
j-tobias <73882406+j-tobias@users.noreply.github.com>
2022-12-20 02:58:46 +0100

    debugging prints added

--- scanner.py
+++ scanner.py
@@ -3,7 +3,7 @@ import json
 import requests
 from datetime import datetime, timedelta
 
-
+print("Setting up ...")
 # get an Initiating Config
 with open("CONFIG.json", mode = "r") as f:
     config = json.load(f)
@@ -22,20 +22,22 @@ api_command                 = config.get("api_command")
 id                          = config.get("ID")
 Measured_Powers             = config.get("Measured Powers")
 
+print("Variables defined")
 
 # initiate the Scanner
 Scanner_ = Scanner(len_value_list, max_timedelta)
-
+print("Scanner set up")
 # initiate the Timers
 update_timer = datetime.now()
 send_timer   = datetime.now()
-
+print("START ENDLESS PROCESS")
 # START THE ENDLESS LOOP
 while True:
 
-
+    print("{: <20} {:<30}".format("Step 1", "check if the config needs to be updated"))
     #check if the config needs to be updated
     if float(timedelta(abs(datetime.now() - update_timer))) >= Frequency_Update_Config:
+        print("{: <20} {:<30}".format("Step 1.1", "Load Config"))
         # --------------------------------------------------------------------------
         # UPDATE ALL VARIABLES FROM THE CONFIG
         with open("CONFIG.json", mode = "r") as f:
@@ -55,34 +57,39 @@ while True:
         id                          = config.get("ID")
         Measured_Powers             = config.get("Measured Powers")
         # --------------------------------------------------------------------------
-        #create an new Scanner Object
+        print("{: <20} {:<30}".format("Step 1.2", "create an new Scanner Object"))
+        # create an new Scanner Object
         Scanner_ = Scanner(len_value_list, max_timedelta)
+        print("{: <20} {:<30}".format("Step 1.3", "update the update_timer"))
         # update the update_timer
         update_timer = datetime.now()
     
+    print("{: <20} {:<30}".format("Step 2", "check if the current scan has to be sent"))
     #check if the current scan has to be sent
     if float(timedelta(abs(datetime.now() - send_timer))) >= Frequency_Send:
-
+        print("{: <20} {:<30}".format("Step 2.1", "Load the current Data"))
         #Load the current Data
         with open(filename_current_data, mode = "r") as f:
             data = json.load(f)
-
+        print("{: <20} {:<30}".format("Step 2.2", "define payload"))
         # define payload
         payload = {id : data}
 
         print(payload)
         # send the Data
         try:
+            print("{: <20} {:<30}".format("Step 2.3", "send the Data"))
             requests.post(f"http://{IP_Adress}:{Port}/{api_command}", json= payload)
         except:
             pass
-
+        print("{: <20} {:<30}".format("Step 2.4", "update the send_timer"))
         # update the send_timer
         send_timer = datetime.now()
 
-    print("Started scanning")
-    # continouisly scan
+    print("{: <20} {:<30}".format("Step 3", "start a scan"))
+    # scan
     Scanner.scan(sleep_between_scans, loop_count, 10)
+    print("{: <20} {:<30}".format("Step 4", "write scanned data to a file"))
     Scanner.get_data(filename_current_data)
 
 
